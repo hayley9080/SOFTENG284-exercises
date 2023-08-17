e7d96b27b69e324234b3064d977864578292215a
hayley9080 <hsmi309@aucklanduni.ac.nz>
2023-08-17 17:01:54 +1200

    Added method 'balance()'
    balance calculates how out-of-balance a node is. This is when the difference between its left and right children are greater than 1.

--- avltrees/Tree.java
+++ avltrees/Tree.java
@@ -28,4 +28,30 @@ public class Tree {
     }
     return false;
   }
+
+  /**
+   * balance is used to calculate how out-of-balance a node is.
+   *
+   * <p>A node is considered out-of-balance if the difference between the height of the left subtree
+   * and the right subtree is greater than 1.
+   *
+   * <p>If positive, left is heavier. If negative, right is heavier.
+   *
+   * @param node the node to check
+   * @return the balance of the node
+   */
+  public int balance(Node node) {
+    int leftHeight = -1;
+    int rightHeight = -1;
+
+    if (node.left != null) {
+      leftHeight = node.left.height;
+    }
+
+    if (node.right != null) {
+      rightHeight = node.right.height;
+    }
+
+    return leftHeight - rightHeight;
+  }
 }
